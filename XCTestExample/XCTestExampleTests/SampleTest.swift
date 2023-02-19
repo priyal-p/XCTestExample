119c897c5df09c9e12b09cc461cbc85d36df0746
Priyal PORWAL <26priyalmrbd@gmail.com>
2023-02-19 15:01:41 +0530

    Remove unused code

--- XCTestExample/XCTestExampleTests/SampleTest.swift
+++ XCTestExample/XCTestExampleTests/SampleTest.swift
@@ -10,6 +10,7 @@ import XCTest
 final class SampleTest: XCTestCase {
 
     let sample = Sample()
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,13 +19,7 @@ final class SampleTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-
+    func testExample() {
         sample.changeState()
 
         XCTAssertEqual(sample.state, 1)
@@ -32,14 +27,8 @@ final class SampleTest: XCTestCase {
 
     func testExample2() {
         sample.changeState()
-        XCTAssertEqual(sample.state, 2)
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+//        XCTAssertEqual(sample.state, 2) // Failing test - XCTAssertEqual failed: ("1") is not equal to ("2")
+        XCTAssertEqual(sample.state, 1)
     }
 
     class Sample {
