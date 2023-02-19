08a06c221d76618e90d158e4166057ffbff35eb0
Priyal Porwal <26priyalmrbd@gmail.com>
2023-02-19 14:44:50 +0530

    Inital Commit

---
+++ XCTestExample/XCTestExampleTests/SampleTest.swift
@@ -0,0 +1,52 @@
+//
+//  SampleTest.swift
+//  XCTestExampleTests
+//
+//  Created by Priyal PORWAL on 19/02/23.
+//
+
+import XCTest
+
+final class SampleTest: XCTestCase {
+
+    let sample = Sample()
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+
+        sample.changeState()
+
+        XCTAssertEqual(sample.state, 1)
+    }
+
+    func testExample2() {
+        sample.changeState()
+        XCTAssertEqual(sample.state, 2)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    class Sample {
+        var state = 0
+
+        func changeState() {
+            state += 1
+        }
+    }
+}
