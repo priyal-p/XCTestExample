dc9c1f610df9e2d6291091e79912314c1835fc0b
Priyal PORWAL <26priyalmrbd@gmail.com>
2023-02-19 15:04:29 +0530

    Using setUp and tearDown

--- XCTestExample/XCTestExampleTests/SampleTest.swift
+++ XCTestExample/XCTestExampleTests/SampleTest.swift
@@ -9,14 +9,17 @@ import XCTest
 
 final class SampleTest: XCTestCase {
 
-    let sample = Sample()
+    var sample: Sample!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sample = Sample()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        print("setup - SampleTest identifier \(ObjectIdentifier(self))")
+        sample = nil
     }
 
     func testExample() {
